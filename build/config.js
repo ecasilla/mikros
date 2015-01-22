0ecc82963f091e0076fe6b06adcee2e17a43b650
Ernie Casilla <ecasilla@icloud.com>
2015-01-22 09:59:26 -0300

    finishing up build and added the inital folder setup

--- build/config.js
+++ build/config.js
@@ -2,5 +2,11 @@ var dest = './dist',
     src  = './lib';
 
 module.exports = {
- 
+  runner:{
+    src:{
+      html: __dirname + 'test/runner.html',
+      js: __dirname + 'test/runner.js',
+      test: __dirname + 'test/spec/**/*js'
+    }
+  } 
 };
--- build/tasks/mocha_runner.js
+++ build/tasks/mocha_runner.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp'),
     inject = require('gulp-inject'),
-    config = require('./config');
+    config = require('../config');
 
 gulp.task('runner',function() {
- var target = gulp.src(config.runner.src);
-})
+ var target = gulp.src(config.runner.src.html);
+ var jsfiles = gulp.src(config.runner.src.js,{read:false});
+ var testfile = gulp.src(config.runner.src.test,{read:false});
+
+ target
+ .pipe(inject(jsfiles,{name:'lib'}))
+ .pipe(inject(testfile,{name:'test'}))
+ .pipe(gulp.dest('./dist'))
+});
